cb7ecd1a257893ab8e4a7ff9ab2344919a68b04c
Paweł Dyczek <paweldyczek02@gmail.com>
2025-07-17 01:00:21 +0200

    SH-160 Moved Config template definitions to .tpp file
    Definition of getValue method moved from header to .tpp file.
    This change helps to keep separation between declarations and definitions for better header readability.

--- central_unit/src/common/utils/config.h
+++ central_unit/src/common/utils/config.h
@@ -56,42 +56,7 @@ namespace SmartHome::Utils {
          * @endcode
          */
         template<typename T>
-        //TODO move definition to .tpp file
-        std::optional<T> getValue(const std::string &valuePath) {
-            if (mIsConfigLoaded.load()) {
-                // Prepare keys from string
-                std::vector<std::string> keys = {};
-                boost::split(keys, valuePath, boost::is_any_of("."));
-
-                std::string currentValuePath;
-                YAML::Node currentNode = YAML::Clone(mConfigNode);
-                // Iterate on keys, check if they are defined
-                for (auto &key: keys) {
-                    currentValuePath += key + ".";
-                    currentNode = currentNode[key];
-
-                    if (!currentNode.IsDefined()) {
-                        // Handle invalid path
-                        if (!currentValuePath.empty()) currentValuePath.pop_back();
-                        std::cerr << "Config getValue() error: value path \"" << currentValuePath << "\" is not defined"
-                                << std::endl;
-                        return std::nullopt;
-                    }
-                }
-                // Catch value conversion errors
-                try {
-                    return currentNode.as<T>();
-                } catch (const std::exception &e) {
-                    currentValuePath.pop_back();
-                    std::cerr << "Config getValue() error: bad conversion on " << currentValuePath << ": " <<
-                            currentNode << std::endl;
-                    return std::nullopt;
-                }
-            }
-            // Handle config not loaded
-            std::cerr << "Config getValue() error: config not loaded" << std::endl;
-            return std::nullopt;
-        }
+        std::optional<T> getValue(const std::string &valuePath);
 
         /**
         * @brief Get value and store in reference.
@@ -109,12 +74,7 @@ namespace SmartHome::Utils {
         * @endcode
         */
         template<typename T>
-        void getValue(const std::string &valuePath, T &value) {
-            auto result = getValue<T>(valuePath);
-            if (result.has_value()) {
-                value = result.value();
-            }
-        }
+        void getValue(const std::string &valuePath, T &value);
 
     private:
         /**
@@ -131,3 +91,5 @@ namespace SmartHome::Utils {
         std::atomic<bool> mIsConfigLoaded{false}; ///< Configuration loaded state.
     };
 }
+
+#include "config.tpp"
