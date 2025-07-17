84706710181ed622c1001becb0ac52de3109001b
Kacper Karasiński <karasinski.kacper@gmail.com>
2025-07-17 14:26:51 +0200

    SH-189 Change "include guards" to "#pragma once"

--- modules/src/config/basic_esp32_wroom_config.h
+++ modules/src/config/basic_esp32_wroom_config.h
@@ -1,20 +1,19 @@
-#ifndef BASIC_ESP32_WROOM_CONFIG_H
-#define BASIC_ESP32_WROOM_CONFIG_H
-    #ifdef HC12_MODULE
-        #define BAUD_RATE 9600
-        #define RX_PIN 25 
-        #define TX_PIN 33
-        #define SET_PIN 32
-        #define HARDWARE_SERIAL_UART_NR 2
-        #define RF_CHANNELS
-        
-        #define SETUP_COMMAND_SIZE 10
-        #define SETUP_MAX_NUM_OF_COMMANDS 5
-        #define SETUP_MAX_LEN_OF_RESPONSE 43
-    #endif
+#pragma once
 
-    #define LED_PIN 26
-    #define BUTTON_PIN 27
+#ifdef HC12_MODULE
+    #define BAUD_RATE 9600
+    #define RX_PIN 25 
+    #define TX_PIN 33
+    #define SET_PIN 32
+    #define HARDWARE_SERIAL_UART_NR 2
+    #define RF_CHANNELS
     
-    // #define CENTRAL_UNIT
+    #define SETUP_COMMAND_SIZE 10
+    #define SETUP_MAX_NUM_OF_COMMANDS 5
+    #define SETUP_MAX_LEN_OF_RESPONSE 43
 #endif
+
+#define LED_PIN 26
+#define BUTTON_PIN 27
+
+// #define CENTRAL_UNIT
--- modules/src/smart_home_config.h
+++ modules/src/smart_home_config.h
@@ -1,18 +1,16 @@
-#ifndef SMART_HOME_CONFIG_H
-#define SMART_HOME_CONFIG_H
-    #define BACKGROUND_TASK_PRIORITY 1
-    #define LOW_TASK_PRIORITY 2
-    #define MEDIUM_TASK_PRIORITY 3
-    #define HIGH_TASK_PRIORITY 4
-    #define CRITICAL_TASK_PRIORITY 5
+#pragma once
 
-    #define HC12_MODULE
+#define BACKGROUND_TASK_PRIORITY 1
+#define LOW_TASK_PRIORITY 2
+#define MEDIUM_TASK_PRIORITY 3
+#define HIGH_TASK_PRIORITY 4
+#define CRITICAL_TASK_PRIORITY 5
+#define HC12_MODULE
 
-    #ifdef ESP32_WROOM_BOARD_TYPE
-        #include "config/basic_esp32_wroom_config.h"
-    #endif
-    #ifdef ESP32_S3_WROOM_BOARD_TYPE
-        #include "config/basic_esp32_s3_wroom_config.h"
-    #endif
+#ifdef ESP32_WROOM_BOARD_TYPE
+    #include "config/basic_esp32_wroom_config.h"
+#endif
 
+#ifdef ESP32_S3_WROOM_BOARD_TYPE
+    #include "config/basic_esp32_s3_wroom_config.h"
 #endif
