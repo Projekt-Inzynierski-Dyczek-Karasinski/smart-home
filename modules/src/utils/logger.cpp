20f90489e73bdefd3cd149292b8cf9aa38ebe600
Kacper Karasiński <karasinski.kacper@gmail.com>
2025-08-07 18:33:14 +0200

    SH-195 Add Logger class and Logging namespace

---
+++ modules/src/utils/logger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by karas on 2025-08-07.
+//
+
+#include "logger.h"
---
+++ modules/src/utils/logger.h
@@ -0,0 +1,13 @@
+//
+// Created by karas on 2025-08-07.
+//
+
+#ifndef MODULES_LOGGER_H
+#define MODULES_LOGGER_H
+
+
+class logger {
+};
+
+
+#endif //MODULES_LOGGER_H
